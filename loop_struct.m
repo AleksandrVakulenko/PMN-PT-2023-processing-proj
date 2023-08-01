f24214a8eea5d18787b6b5f5b9b3ea2073e08190
Aleksandr Vakulenko <sasha705@mail.ru>
2023-08-01 13:30:45 +0300

    add loop struct plan

---
+++ loop_struct.m
@@ -0,0 +1,57 @@
+
+%       VERSION
+%
+
+%       ENVIRONMENT
+% time of measurement start
+% temperature
+% measuring device
+% amplifier
+%
+
+%       SAMPLE
+% thickness
+% surface area
+% material
+% ID
+% 
+
+%       LOOP PROPs
+% measuriong device
+% waveform ??(legacy)
+% pulse sequence
+% period
+% amplitude
+% dain
+% divider
+% amplifier BW
+% 
+
+
+%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
